d89e51efd5a39ca827132a93642f899426e82cbd
kate <jjhou919@gmail.com>
2025-12-09 12:54:26 +0800

    A-branch aweb update1

--- homework1/urls.py
+++ homework1/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# A
 from django.contrib import admin
 from django.urls import path
 from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', views.test, name='test'),
+    path('aweb/', views.aweb, name='aweb'),
 ]
+ 
